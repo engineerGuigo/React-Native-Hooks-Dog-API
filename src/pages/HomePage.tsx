9fae19ec2cc05d54325c554d461e0932130a3123
Rodrigo Durante <rodrigodurante@Guilhermes-MacBook-Pro.local>
2020-02-27 01:13:55 -0300

    Object properly displayed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import {SafeAreaView, Text, Image} from 'react-native';
+import React, { useState } from 'react';
+import {SafeAreaView, Text, FlatList, Image, View} from 'react-native';
+import axios from 'axios';
+
 
 const HomePage = () => {
+
+    const [breeds, setBreeds] = useState({});
+    axios
+    .get('https://dog.ceo/api/breeds/list/all')
+    .then(({data}) => {
+        const breedsObject = data.message;
+        const breedKeys = Object.keys(breedsObject);
+        const assembledBreedsObject = {};
+        breedKeys.map(key => {
+            if(breedsObject[key].length > 0){
+                // the breed has sub breeds
+                    breedsObject[key].forEach(subBreed => {
+                        assembledBreedsObject[key + '_' + subBreed] = key + '/' + subBreed;
+                    });
+            } else {
+                // this breed has NO sub breeds
+                assembledBreedsObject[key] = key;
+            }
+        });
+
+        setBreeds(assembledBreedsObject);
+    });
+
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center'
+        }}>
+            <View style={{flex: 1}}>
             <Image source={ require('../../images/vavau.png')}/>
+            </View>
+            <View style={{flex: 4}}>
+                <Text>{JSON.stringify(breeds)}</Text>
+            </View>
         </SafeAreaView>
     );
 };
